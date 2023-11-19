3e661a96c7afa0f7c63ac0f56380091c04a0eafa
IhorSlakva <143197433+IhorSlakva@users.noreply.github.com>
2023-11-19 16:34:47 +0200

    homework-fix

--- js/task-4.js
+++ js/task-4.js
@@ -21,9 +21,9 @@ function getShippingCost(country) {
     return message;
 }
 
-console.log(getShippingCost("Australia"));
-console.log(getShippingCost("Germany"));
-console.log(getShippingCost("China"));
-console.log(getShippingCost("Chile"));
-console.log(getShippingCost("Jamaica"));
-console.log(getShippingCost("Sweden"));
+console.log(getShippingCost('Australia'));
+console.log(getShippingCost('Germany'));
+console.log(getShippingCost('China'));
+console.log(getShippingCost('Chile'));
+console.log(getShippingCost('Jamaica'));
+console.log(getShippingCost('Sweden'));
